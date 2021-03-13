cb23325465b883599a5a16430346f5d987c2fa89
kavukj <jainkavya6890@gmail.com>
2021-03-14 00:29:30 +0530

    set condition for logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,10 @@ import rootReducer from '../redux/root.reducer';
 import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 
-const middlewares = [logger];
+const middlewares = [];
+if(process.env.NODE_ENV === 'development'){
+    middlewares.push(logger);
+}
 
 export const store =  createStore(rootReducer,applyMiddleware(...middlewares));
 
